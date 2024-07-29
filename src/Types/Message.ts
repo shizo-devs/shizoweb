16e6c89f7c2775624b0faac34a3c6c9f13e65330
Shizo The Techie <98589136+shizothetechie@users.noreply.github.com>
2024-07-29 20:53:46 +0530

    Update Message.ts

--- src/Types/Message.ts
+++ src/Types/Message.ts
@@ -174,13 +174,13 @@ export type AnyRegularMessageContent = (
     | {
         listReply: Omit<proto.Message.IListResponseMessage, 'contextInfo'>
     }
-    | { 
-        pin: WAMessageKey,
-        time: 86400 | 604800 | 2592000
-    }
-    |
-    { 
-        unpin: WAMessageKey
+    | {
+        pin: WAMessageKey
+        type: proto.PinInChat.Type
+        /**
+         * 24 hours, 7 days, 30 days
+         */
+        time?: 86400 | 604800 | 2592000
     }
     | {
         product: WASendableProduct
